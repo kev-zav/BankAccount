726688cea6950f148067b5b3c51d832408b597cf
kev-zav <zavalak2811@gmail.com>
2026-06-29 00:05:17 -0500

    Started Main.java

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.Exception;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.println("1. View customer profile");
+            System.out.println("2. Open account");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Deposit");
+            System.out.println("5. Transfer funds");
+            System.out.println("6. Complete and exit");
+            int choice = scanner.nextInt();
+
+
+            switch (choice) {
+                case 1: 
+            }
+            
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
