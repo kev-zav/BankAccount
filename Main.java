3f22483f7d4905cb0c9c56836af674e8f256ae35
kev-zav <zavalak2811@gmail.com>
2026-06-30 01:51:17 -0500

    Added case 1 and case 2. Viewing a customer and creating a new customer

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.Exception;
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,18 +19,72 @@ public class Main {
 
             switch (choice) {
                 case 1: 
+                    System.out.println("Enter a CIF number");
+                    String searchCif = scanner.nextLine();
+                    for (Customer c : customers) {
+                        if (c.getCif().equals(searchCif)) {
+                            System.out.println(c.getCif());
+                            System.out.println(c.getName());
+                            System.out.println(c.getDob());
+                            for (Account a : c.getAccount()) {
+                                System.out.println(a.getAccountNumber() + " " + a.getAccountType() + ": " + a.getBalance());
+                                System.out.println();
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter a CIF number to assign the customer: ");
+                    boolean found = false;
+                    searchCif = scanner.nextLine();
+                        for (Customer c : customers) {
+                            if (c.getCif().equals(searchCif)) {
+                                found = true;
+                            }
+                        }
+                        if (found) {
+                            System.out.println("CIF number is invalid or unavailable.");
+                            break;
+                        }
+                        else {
+                            System.out.println("Enter customer name: ");
+                            String name = scanner.nextLine();
+                            System.out.println("Enter customer date of birth: ");
+                            String dob = scanner.nextLine();
+                            Customer c = new Customer(name, searchCif, dob);
+                            customers.add(c);
+                            System.out.println("What account would the customer like to open: ");
+                            String accountType = scanner.nextLine();
+                            System.out.println("Assign a 9 digit number to the account");
+                            String searchAccountNumber = scanner.nextLine();
+                            found = false;
+                            if (searchAccountNumber.length() != 9) {
+                                found = true;
+                                System.out.println("Account number is not 9 digits.");
+                                break;
+                            }
+                            for (Customer c2 : customers) {
+                                for (Account a : c2.getAccount()) {
+                                    if (a.getAccountNumber().equals(searchAccountNumber)) {
+                                        found =true;
+                                    }
+                                }
+                            }
+                            if (found) {
+                                System.out.println("Account number is unavailable");
+                                break;        
+                            }
+                            else {
+                                System.out.println("What is the opening balance of the account?");
+                                double balance = scanner.nextDouble();
+                                Account a = new Account(balance, accountType, searchAccountNumber);
+                                c.addAccount(a);
+                                break;
+                        }
+                    }
+                }
             }
-            
-        
-
-
-
-
-
-
-
-
-
-
+            scanner.close();
+        }
     }
-}
